87a4e8fc7b699ac34b6ed872b6503e19d42c4b23
ahindy1234 <71048177+ahindy1234@users.noreply.github.com>
2021-06-02 12:02:01 -0400

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/ahindy1234/ahindy1234.github.io.git";
 
 ghpages.publish(
   pathname,
